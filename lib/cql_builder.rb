5e3596170f8224c8f924d47d334c3ffa8d2c9449
Andrew Kozin <andrew.kozin@gmail.com>
2015-08-10 21:47:01 +0300

    [FEATURE] Add `Operators` module
    The collection of CQL operators used in clauses.
    
    == Added
    
    * CQLBuilder::Operators

--- lib/cql_builder.rb
+++ lib/cql_builder.rb
@@ -6,6 +6,7 @@ require "ice_nine"
 require_relative "cql_builder/base"
 require_relative "cql_builder/clause"
 require_relative "cql_builder/statement"
+require_relative "cql_builder/operators"
 
 # The builder of CQL statements
 #
---
+++ lib/cql_builder/operators.rb
@@ -0,0 +1,11 @@
+# encoding: utf-8
+
+module CQLBuilder
+
+  # The collection of operators
+  #
+  module Operators
+
+  end # module Operators
+
+end # module CQLBuilder
