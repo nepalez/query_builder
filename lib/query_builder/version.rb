6da777ee219265e0d311e6105557cbc54fde9428
Danilo Caballero Chinchilla <danilo@easypost.com>
2021-03-06 00:19:05 +0000

    Bump to 0.0.4.ep

--- lib/query_builder/version.rb
+++ lib/query_builder/version.rb
@@ -5,6 +5,6 @@ module QueryBuilder
 
   # The semantic version of the module.
   # @see http://semver.org/ Semantic versioning 2.0
-  VERSION = "0.1.0"
+  VERSION = "0.0.4.ep".freeze
 
 end # module QueryBuilder
